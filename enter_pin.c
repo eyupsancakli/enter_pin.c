92f691637b0599935104f9d0f7c3c692a9f4b693
Eyup Sancakli <82240491+eyupsancakli@users.noreply.github.com>
2023-12-09 12:35:11 +0000

    Add files via upload

---
+++ enter_pin.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+  int pin = 0;
+  int tries = 0;
+
+  printf("BANK OF CODECADEMY:\n");
+  printf("Enter your PIN: ");
+  scanf("%d", &pin);
+
+  tries++;
+
+  while (pin != 1234 && tries < 3) {
+    printf("Enter your PIN: ");
+    scanf("%d", &pin);
+    tries++;
+  }
+
+  if (pin == 1234) {
+    printf("PIN accepted!\n");
+    printf("You now have access.\n");
+  }
+  else if (pin != 1234){
+    printf("Your passoword is not correct!");
+  }
+
+}
